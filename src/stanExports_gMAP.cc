2832d9760827b7f07f1f233c7713e2efa6aa21a9
Sebastian Weber <sebastian.weber@novartis.com>
2023-06-29 10:10:02 +0000

    version 1.6-7

--- src/stanExports_gMAP.cc
+++ src/stanExports_gMAP.cc
@@ -7,7 +7,7 @@ using namespace Rcpp ;
 RCPP_MODULE(stan_fit4gMAP_mod) {
 
 
-    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("model_gMAP")
+    class_<rstan::stan_fit<stan_model, boost::random::ecuyer1988> >("rstantools_model_gMAP")
 
     .constructor<SEXP,SEXP,SEXP>()
 
